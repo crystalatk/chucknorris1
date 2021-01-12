f663c40896afbb92c0c6e501a9733baae585f254
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-01-12 10:01:18 -0500

    Added html and js files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+'use strict';
+
+fetch("https://api.chucknorris.io/jokes/random?category=dev")
+.then(function (response) {
+    console.log(response);
+    return response.json();
+})
+.then(function(data) {
+    updateBody(data.value);
+})
+.catch(function(error){
+    console.error("ERROR:", error);
+    return error;
+});
+
+function updateBody(quote) {
+    const chuckSays = document.querySelector("#chuckSays");
+    chuckSays.innerHTML = quote;
+}
